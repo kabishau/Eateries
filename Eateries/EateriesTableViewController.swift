3f2b09204281e39ad029007d4c5e69a00cf52a5d
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-10-21 19:38:41 -0400

    struct Restaurant has been added refactoring data storage from multiple array to one array that store instances of the struct

--- Eateries/EateriesTableViewController.swift
+++ Eateries/EateriesTableViewController.swift
@@ -10,11 +10,22 @@ import UIKit
 
 class EateriesTableViewController: UITableViewController {
     
-    var restaurantNames = ["Ogonёk Grill&Bar", "Елу", "Bonsai", "Дастархан", "Индокитай", "X.O", "Балкан Гриль", "Respublica", "Speak Easy", "Morris Pub", "Вкусные истории", "Классик", "Love&Life", "Шок", "Бочка"]
-    
-    var restaurantImages = ["ogonek.jpg", "elu.jpg", "bonsai.jpg", "dastarhan.jpg", "indokitay.jpg", "x.o.jpg", "balkan.jpg", "respublika.jpg", "speakeasy.jpg", "morris.jpg", "istorii.jpg", "klassik.jpg", "love.jpg", "shok.jpg", "bochka.jpg"]
-    
-    var restaurantIsVisited = [Bool](repeatElement(false, count: 15))
+    var restaurants: [Restaurant] = [
+        Restaurant(name: "Ogonёk Grill&Bar", type: "ресторан", location: "Уфа", image: "ogonek.jpg", isVisited: false),
+        Restaurant(name: "Елу", type: "ресторан", location: "Уфа", image: "elu.jpg", isVisited: false),
+        Restaurant(name: "Bonsai", type: "ресторан", location: "Уфа", image: "bonsai.jpg", isVisited: false),
+        Restaurant(name: "Дастархан", type: "ресторан", location: "Уфа", image: "dastarhan.jpg", isVisited: false),
+        Restaurant(name: "Индокитай", type: "ресторан", location: "Уфа", image: "indokitay.jpg", isVisited: false),
+        Restaurant(name: "X.O", type: "ресторан-клуб", location: "Уфа", image: "x.o.jpg", isVisited: false),
+        Restaurant(name: "Балкан Гриль", type: "ресторан", location: "Уфа", image: "balkan.jpg", isVisited: false),
+        Restaurant(name: "Respublica", type: "ресторан", location: "Уфа", image: "respublika.jpg", isVisited: false),
+        Restaurant(name: "Speak Easy", type: "ресторанный комплекс", location: "Уфа", image: "speakeasy.jpg", isVisited: false),
+        Restaurant(name: "Morris Pub", type: "ресторан", location: "Уфа", image: "morris.jpg", isVisited: false),
+        Restaurant(name: "Вкусные истории", type: "ресторан", location: "Уфа", image: "istorii.jpg", isVisited: false),
+        Restaurant(name: "Классик", type: "ресторан", location: "Уфа", image: "klassik.jpg", isVisited: false),
+        Restaurant(name: "Love&Life", type: "ресторан", location: "Уфа", image: "love.jpg", isVisited: false),
+        Restaurant(name: "Шок", type: "ресторан", location: "Уфа", image: "shok.jpg", isVisited: false),
+        Restaurant(name: "Бочка", type: "ресторан", location:  "Уфа", image: "bochka.jpg", isVisited: false)]
     
     
     override func viewDidLoad() {
@@ -25,37 +36,39 @@ class EateriesTableViewController: UITableViewController {
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
-        // #warning Incomplete implementation, return the number of sections
         return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
-        return restaurantNames.count
+        return restaurants.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! EateriesTableViewCell
         
-        cell.nameLabel.text = restaurantNames[indexPath.row]
-        cell.thumbnailImageView.image = UIImage(named: restaurantImages[indexPath.row])
+        cell.nameLabel.text = restaurants[indexPath.row].name
+        cell.thumbnailImageView.image = UIImage(named: restaurants[indexPath.row].image)
         // making images round
         cell.thumbnailImageView.layer.cornerRadius = 32.5
         cell.thumbnailImageView.clipsToBounds = true
         
         // ternary operator - replacement of if-else statement
-        cell.accessoryType = self.restaurantIsVisited[indexPath.row] ? .checkmark : .none
+        cell.accessoryType = self.restaurants[indexPath.row].isVisited ? .checkmark : .none
 
 
         return cell
     }
 
-
+/*
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         let alertController = UIAlertController(title: nil, message: "Choose action", preferredStyle: .actionSheet)
         
+        // cancel action
         let cancel = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+        alertController.addAction(cancel)
+        
+        // call action
         let call = UIAlertAction(title: "Call 215-966-956\(indexPath.row)", style: .default) {
             (action: UIAlertAction) -> Void in
             let alertController = UIAlertController(title: nil, message: "Call can not be done", preferredStyle: .alert)
@@ -63,30 +76,29 @@ class EateriesTableViewController: UITableViewController {
             alertController.addAction(okay)
             self.present(alertController, animated: true, completion: nil)
         }
+        alertController.addAction(call)
         
-        let visitedTitle = self.restaurantIsVisited[indexPath.row] ? "Check out" : "Check In"
+        // check-in/check-out action
+        let visitedTitle = self.restaurants[indexPath.row].isVisited ? "Check out" : "Check In"
         let isVisited = UIAlertAction(title: visitedTitle, style: .default) { (action) in
 
             let cell = tableView.cellForRow(at: indexPath)
-            self.restaurantIsVisited[indexPath.row] = !self.restaurantIsVisited[indexPath.row]
-            cell?.accessoryType = self.restaurantIsVisited[indexPath.row] ? .checkmark : .none
+            self.restaurants[indexPath.row].isVisited = !self.restaurants[indexPath.row].isVisited
+            cell?.accessoryType = self.restaurants[indexPath.row].isVisited ? .checkmark : .none
         }
-        
-        alertController.addAction(cancel)
-        alertController.addAction(call)
         alertController.addAction(isVisited)
+        
+        
         present(alertController, animated: true, completion: nil)
         
         // deselecting standart highlighting
         tableView.deselectRow(at: indexPath, animated: true)
         
     }
-    
+*/
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
-            restaurantNames.remove(at: indexPath.row)
-            restaurantImages.remove(at: indexPath.row)
-            restaurantIsVisited.remove(at: indexPath.row)
+            restaurants.remove(at: indexPath.row)
         }
         tableView.deleteRows(at: [indexPath], with: .fade)
     }
---
+++ Eateries/Restaurants.swift
@@ -0,0 +1,18 @@
+//
+//  Restaurants.swift
+//  Eateries
+//
+//  Created by Aleksey Kabishau on 1021..17.
+//  Copyright © 2017 Aleksey Kabishau. All rights reserved.
+//
+
+import Foundation
+
+struct Restaurant {
+    var name: String
+    var type: String
+    var location: String
+    var image: String
+    var isVisited: Bool
+    
+}
