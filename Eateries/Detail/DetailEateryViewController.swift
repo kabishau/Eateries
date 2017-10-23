3d6fd21a392e040e1150deceb8d61105e1bcfdb8
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-10-22 20:52:20 -0400

    File was renamed

---
+++ Eateries/Detail/DetailEateryViewController.swift
@@ -0,0 +1,67 @@
+//
+//  DetailEateryViewController.swift
+//  Eateries
+//
+//  Created by Aleksey Kabishau on 1021..17.
+//  Copyright © 2017 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class DetailEateryViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var restaurant: Restaurant!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        imageView.image = UIImage(named: restaurant.image)
+        
+        // table view customization
+        // removing the rest of the unused tableView
+        tableView.tableFooterView = UIView(frame: CGRect.zero)
+        
+        
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 4
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! DetailEateryTableViewCell
+        
+        switch indexPath.row {
+        case 0:
+            cell.keyLabel.text = "Name"
+            cell.valueLabel.text = restaurant.name
+        case 1:
+            cell.keyLabel.text = "Type"
+            cell.valueLabel.text = restaurant.type
+        case 2:
+            cell.keyLabel.text = "Location"
+            cell.valueLabel.text = restaurant.location
+        case 3:
+            cell.keyLabel.text = "Is Visited?"
+            cell.valueLabel.text = restaurant.isVisited ? "Yes" : "No"
+        default:
+            break
+        }
+        
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+}
