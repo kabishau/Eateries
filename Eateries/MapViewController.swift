54f983559899a5e3397dfb9c9970555925ac98d9
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-11-03 22:29:10 -0400

    Show selected Item on map using CLGeocoder and MKPointAnnotation

--- Eateries/MapViewController.swift
+++ Eateries/MapViewController.swift
@@ -18,7 +18,25 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        // instance of Core Location Geocoder Class
+        let geocoder = CLGeocoder()
+        geocoder.geocodeAddressString(restaurant.location) { (placemarks, error) in
+            guard error == nil else { return }
+            guard let placemarks = placemarks else { return }
+            let placemark = placemarks.first!
+            
+            // instance of MKPointAnnotation Class - pin on map
+            let annotation = MKPointAnnotation()
+            annotation.title = self.restaurant.name
+            annotation.subtitle = self.restaurant.type
+            
+            guard let location = placemark.location else { return }
+            annotation.coordinate = location.coordinate
+            // presenting on the map
+            self.mapView.showAnnotations([annotation], animated: true)
+            self.mapView.selectAnnotation(annotation, animated: true)
+            
+        }
 
     }
 
