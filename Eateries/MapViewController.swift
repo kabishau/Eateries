4f21025b596236e1293fbeb85bb2443e16dbca5b
user131656 <user131656@ny218.local>
2017-11-04 12:46:55 -0400

    Setting Annotations for selected address Pin is now displayed for address.

--- Eateries/MapViewController.swift
+++ Eateries/MapViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import MapKit
 
 
-class MapViewController: UIViewController {
+class MapViewController: UIViewController, MKMapViewDelegate {
     
     var restaurant: Restaurant!
 
@@ -18,6 +18,9 @@ class MapViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        mapView.delegate = self
+        
         // instance of Core Location Geocoder Class
         let geocoder = CLGeocoder()
         geocoder.geocodeAddressString(restaurant.location) { (placemarks, error) in
@@ -38,6 +41,32 @@ class MapViewController: UIViewController {
             
         }
 
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        
+        guard !(annotation is MKUserLocation) else { return nil }
+        
+        // to see Pin on the map we have to use MKPinAnnotationView, not just MKAnnotationView
+        let annotationIdentifier = "restaurantAnnotation"
+        var annotationView = mapView.dequeueReusableAnnotationView(withIdentifier: annotationIdentifier) as? MKPinAnnotationView
+        
+        // if creation of annotation through the dequeueReusable method was not successful, this view should be created manually
+        if annotationView == nil {
+            annotationView = MKPinAnnotationView(annotation: annotation, reuseIdentifier: annotationIdentifier)
+            annotationView?.canShowCallout = true
+        }
+        
+        // adding image to the right side of annotation
+        let rightImage = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        rightImage.image = UIImage(named: restaurant.image)
+        annotationView?.rightCalloutAccessoryView = rightImage
+        
+        // pin customization
+        annotationView?.pinTintColor = #colorLiteral(red: 0.9372549057, green: 0.3490196168, blue: 0.1921568662, alpha: 1)
+        
+        return annotationView
+        
     }
 
 }
