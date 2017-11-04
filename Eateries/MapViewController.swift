af813326f352c435d176dfc241f9fbdae960db27
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-11-03 21:37:31 -0400

    MapViewController has been created. Transition to new screen through Show Segue

---
+++ Eateries/MapViewController.swift
@@ -0,0 +1,23 @@
+//
+//  MapViewController.swift
+//  Eateries
+//
+//  Created by Aleksey Kabishau on 1103..17.
+//  Copyright © 2017 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+import MapKit
+
+
+class MapViewController: UIViewController {
+
+    @IBOutlet weak var mapView: MKMapView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+
+    }
+
+}
