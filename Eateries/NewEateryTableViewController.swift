76b3f654f6abecc34af814226435f7359be058f3
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-11-05 00:58:49 -0400

    Alert Controller for using image picker (camera, photo or cancel options) without functionality

--- Eateries/NewEateryTableViewController.swift
+++ Eateries/NewEateryTableViewController.swift
@@ -17,7 +17,24 @@ class NewEateryTableViewController: UITableViewController {
 
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        //
+        
+        if indexPath.row == 0 {
+            let alertController = UIAlertController(title: "Image Source", message: nil, preferredStyle: .actionSheet)
+            let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { (action) in
+                
+            })
+            let photoAction = UIAlertAction(title: "Photo", style: .default, handler: { (action) in
+                
+            })
+            let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
+            alertController.addAction(cameraAction)
+            alertController.addAction(photoAction)
+            alertController.addAction(cancelAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+            
+        }
         
         tableView.deselectRow(at: indexPath, animated: true)
     }
