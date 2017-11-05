0e874ec9b2e3dcd04d39766338f0f6b76668f709
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-11-05 01:44:19 -0400

    Record in Info.plist about camera and photo library usage Method for choosing source for Image Picker. Implementation is in action section of Alert Controller.

--- Eateries/NewEateryTableViewController.swift
+++ Eateries/NewEateryTableViewController.swift
@@ -21,24 +21,31 @@ class NewEateryTableViewController: UITableViewController {
         if indexPath.row == 0 {
             let alertController = UIAlertController(title: "Image Source", message: nil, preferredStyle: .actionSheet)
             let cameraAction = UIAlertAction(title: "Camera", style: .default, handler: { (action) in
-                
+                self.chooseImagePickerAction(source: .camera)
             })
             let photoAction = UIAlertAction(title: "Photo", style: .default, handler: { (action) in
-                
+                self.chooseImagePickerAction(source: .photoLibrary)
             })
             let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
-            
             let actions = [cameraAction, photoAction, cancelAction]
             for action in actions {
                 alertController.addAction(action)
             }
-            
             present(alertController, animated: true, completion: nil)
-            
-            
         }
         
         tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    // function for action closure in camera and photo alert controller actions
+    func chooseImagePickerAction(source: UIImagePickerControllerSourceType) {
+        // checking is there source (camera and photo) on device - can be device without them?
+        if UIImagePickerController.isSourceTypeAvailable(source) {
+            let imagePicker = UIImagePickerController()
+            imagePicker.allowsEditing = true
+            imagePicker.sourceType = source
+            self.present(imagePicker, animated: true, completion: nil)
+        }
     }
 
 }
