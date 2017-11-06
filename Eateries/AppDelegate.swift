f6d65e6044c136d632558d396ab4c6a12d22ab36
user131656 <user131656@ny218.local>
2017-11-06 15:47:01 -0500

    Creating separate class for managing CoreData (copied from AppDelegate - preset template from new project for CoreData) Connecting AppDelegate and CoreDataStack class

--- Eateries/AppDelegate.swift
+++ Eateries/AppDelegate.swift
@@ -12,6 +12,8 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
+    
+    lazy var conreDataStack = CoreDataStack()
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
@@ -54,6 +56,9 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         print("applicationWillTerminate")
+        
+        self.conreDataStack.saveContext()
+        
     }
 
 
