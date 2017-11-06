086d1f098ca9c8905414b4d197394009cdbb2bf8
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2017-11-05 22:59:57 -0500

    Save button has been added on New Item Screen. Save button allows to make transition back to main screen only if all text fields are populated. IsVisited button changes color depending on selection.

--- Eateries/NewEateryTableViewController.swift
+++ Eateries/NewEateryTableViewController.swift
@@ -11,17 +11,41 @@ import UIKit
 class NewEateryTableViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var restaurantTextField: UITextField!
+    @IBOutlet weak var addressTextField: UITextField!
+    @IBOutlet weak var typeTextField: UITextField!
+    @IBOutlet weak var yesButton: UIButton!
+    @IBOutlet weak var noButton: UIButton!
+    
+    @IBAction func isVisitedButtonsTapped(_ sender: UIButton) {
+        if sender == yesButton {
+            sender.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+            noButton.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        } else {
+            sender.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+            yesButton.backgroundColor = #colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)
+        }
+    }
+    
+    @IBAction func saveButtonTapped(_ sender: UIBarButtonItem) {
+        if restaurantTextField.text == "" || addressTextField.text == "" || typeTextField.text == "" {
+            print("not all fields are filled in")
+        } else {
+            performSegue(withIdentifier: "unwindSegueFromNewEatery", sender: self)
+        }
+    }
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        yesButton.backgroundColor = #colorLiteral(red: 0.3411764801, green: 0.6235294342, blue: 0.1686274558, alpha: 1)
+        noButton.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
     }
 
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         // ...EditedImage - to have ability save edited image
         imageView.image = info[UIImagePickerControllerEditedImage] as? UIImage
-        imageView.contentMode = .scaleAspectFit
+        imageView.contentMode = .scaleAspectFill
         imageView.clipsToBounds = true
         dismiss(animated: true, completion: nil)
     }
