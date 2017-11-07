a4af047e811c9cf0a3a11dbaff53f030b381f356
user131656 <user131656@ny218.local>
2017-11-07 17:57:48 -0500

    Creating variables for holding data in Page View Controller. Adding new images for Pages.

--- Eateries/Page/PageViewController.swift
+++ Eateries/Page/PageViewController.swift
@@ -9,6 +9,10 @@
 import UIKit
 
 class PageViewController: UIPageViewController {
+    
+    var headersArray = ["Create", "Find"]
+    var subHeaderArray = ["Create a list of your favourite restaurants", "Find and check on map your favourite restaurants"]
+    var imagesArray = ["food", "iphoneMap"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
