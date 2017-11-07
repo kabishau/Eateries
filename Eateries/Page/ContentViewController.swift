7d27779ddf153026e0acc939b601fbe423a72a77
user131656 <user131656@ny218.local>
2017-11-07 17:33:00 -0500

    Page and Content View Controllers have been created

---
+++ Eateries/Page/ContentViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ContentViewController.swift
+//  Eateries
+//
+//  Created by user131656 on 11/7/17.
+//  Copyright © 2017 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+
+    
+}
---
+++ Eateries/Page/PageViewController.swift
@@ -0,0 +1,18 @@
+//
+//  PageViewController.swift
+//  Eateries
+//
+//  Created by user131656 on 11/7/17.
+//  Copyright © 2017 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class PageViewController: UIPageViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
